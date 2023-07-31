b89cbdb41c98a5a37a2ff2f16887387aaf15d929
zhangchaopei <2251745121@qq.com>
2023-07-31 14:40:16 +0800

    leetcode 63. 不同路径ii

---
+++ src/main/java/leetcode/_100/_063UniquePathsii.java
@@ -0,0 +1,142 @@
+package leetcode._100;
+
+import leetcode.LeetCodeConstant;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.cn/problems/unique-paths-ii/">63. 不同路径 II</a>
+ */
+@LeetCodeConstant.Tag(tagEnums = {
+        LeetCodeConstant.TagEnum.DynamicPrograming,
+})
+public class _063UniquePathsii {
+
+
+    private Solution solution;
+
+    @BeforeEach
+    public void loadSolution() {
+        solution = Factory.getSolution(Policy.DP2);
+    }
+
+
+    @Test
+    public void case0() {
+        Assertions.assertEquals(1, solution.uniquePathsWithObstacles(new int[][]{{0}}));
+    }
+
+    @Test
+    public void case1() {
+        Assertions.assertEquals(1, solution.uniquePathsWithObstacles(new int[][]{{0, 1}, {0, 0}}));
+    }
+
+
+    /**
+     * 自己写的那个耗时 19s
+     *
+     * 官方的 23ms
+     *
+     * 差了多少倍来着 19000/23
+     */
+    @Test
+    public void case2() {
+        Assertions.assertEquals(718991952, solution.uniquePathsWithObstacles(new int[][]{{0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0}, {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0}, {1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0}, {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}}));
+    }
+
+
+    enum Policy {
+        DP,
+        DP2,
+        ;
+    }
+
+    public static class DP implements Solution {
+
+
+        @Override
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            return availablePathCount(obstacleGrid, 0, 0);
+        }
+
+        private int availablePathCount(int[][] obstacleGrid, int x, int y) {
+            if (obstacleGrid[x][y] == 1)
+                return 0;
+
+            if (x == obstacleGrid.length - 1 && y == obstacleGrid[0].length - 1)
+                return 1;
+
+            int downPlans = 0;
+            if (x < obstacleGrid.length - 1)
+                downPlans = availablePathCount(obstacleGrid, x + 1, y);
+
+            int rightPlans = 0;
+            if (y < obstacleGrid[0].length - 1)
+                rightPlans = availablePathCount(obstacleGrid, x, y + 1);
+
+
+            return downPlans + rightPlans;
+        }
+    }
+
+    public static class DP2 implements Solution {
+
+        Integer[][] dp = new Integer[100][100];// [0][0] 到达 [下标+1][下标+1] 的方案数
+
+        @Override
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0 && j == 0)
+                        continue;
+                    if (obstacleGrid[i][j] == 1)
+                        dp[i][j] = 0;
+                    else {
+                        dp[i][j] = (j >= 1 ? dp[i][j - 1] : 0)
+                                + (i >= 1 ? dp[i - 1][j] : 0);
+                    }
+                }
+            }
+
+            return dp[m - 1][n - 1];
+        }
+
+    }
+
+
+    interface Solution {
+        /**
+         * m == obstacleGrid.length
+         * n == obstacleGrid[i].length
+         * 1 <= m, n <= 100
+         * obstacleGrid[i][j] 为 0 或 1
+         */
+        int uniquePathsWithObstacles(int[][] obstacleGrid);
+    }
+
+
+    public static class Factory {
+
+        private static final Map<Policy, Solution> solutionMap = new HashMap<>();
+
+        static {
+            solutionMap.put(Policy.DP, new DP());
+            solutionMap.put(Policy.DP2, new DP2());
+        }
+
+        public static Solution getSolution(Policy policy) {
+            return solutionMap.get(policy);
+        }
+
+    }
+
+
+}
