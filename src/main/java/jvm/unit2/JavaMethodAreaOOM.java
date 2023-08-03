8e483285f8a15028957e0eb0d04785f89fd667fe
zhangchaopei <2251745121@qq.com>
2023-08-03 17:53:02 +0800

    jvm 方法区

--- src/main/java/jvm/unit2/JavaMethodAreaOOM.java
+++ src/main/java/jvm/unit2/JavaMethodAreaOOM.java
@@ -12,8 +12,10 @@ import java.util.List;
  */
 public class JavaMethodAreaOOM {
     /**
+     * HotSpot虚拟机在jdk8前使用永久代（堆内，使得可以像管理堆一样来管理方法区）来实现方法区，jdk8开始使用元空间Metaspace（堆外内存）
+     * 字符串常量池也是，自jdk7起，之前在永久代内会受到 PermSize 限制，后面直接放到堆里面了
      * -XX:PermSize=10M -XX:MaxPermSize=10M
-     * -XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M jdk8 永久代
+     * -XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M
      *
      * @param args
      */
